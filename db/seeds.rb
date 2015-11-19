3e6e4f035fc7ed044dbc4f23e5c013a866aed090
NAwal <ifferreira@uc.cl>
2015-11-18 23:12:50 -0300

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,11 @@ source.url = 'http://www.latercera.com/'
 source.description = 'El diario La Tercera en su version digital.'
 source.save
 
-
+source = Source.new
+source.name = 'Cooperativa'
+source.url = 'http://www.cooperativa.cl/'
+source.description = 'Cooperativa version online'
+source.save
 
 
 # (1..10).each do |i|
@@ -51,25 +55,25 @@ source.save
 
 
 
-(1..10).each do |i|
-	Tag.create({
-		tag_name: "Tag #{i}"
-	})
-end
+# (1..10).each do |i|
+# 	Tag.create({
+# 		tag_name: "Tag #{i}"
+# 	})
+# end
 
-image = ["http://hordago.org/wp-content/uploads/2012/10/procesoConstituyente.jpg",
-		"http://s3.amazonaws.com/latetera/app/public/system/photos/4671/original/michelle-bachelet-2013_195740.jpg",
-		"http://s3.amazonaws.com/latetera/app/public/system/photos/5716/original/Pi%C3%B1era_dinosaurio.jpg?1438561568",
-		"http://www.revistamercado.do/app2/wp-content/uploads/images/imagen-CHILE.jpg"]
+# image = ["http://hordago.org/wp-content/uploads/2012/10/procesoConstituyente.jpg",
+# 		"http://s3.amazonaws.com/latetera/app/public/system/photos/4671/original/michelle-bachelet-2013_195740.jpg",
+# 		"http://s3.amazonaws.com/latetera/app/public/system/photos/5716/original/Pi%C3%B1era_dinosaurio.jpg?1438561568",
+# 		"http://www.revistamercado.do/app2/wp-content/uploads/images/imagen-CHILE.jpg"]
 
-(1..20).each do |i|
-	NewsItem.create({
-		title: "Noticia #{i}",
-		url: "http://www.noticia#{1}.cl",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat vulputate lobortis. Etiam eleifend cursus orci, sed lobortis quam blandit eget. Nulla ac augue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam tempor lacinia mauris et placerat. Aenean finibus nec magna a pharetra. Morbi at imperdiet risus. Pellentesque tincidunt vestibulum nibh id bibendum. Curabitur nec lobortis nulla. Nullam aliquam vestibulum nisl eu dapibus. Pellentesque quis nulla vitae tortor sollicitudin fringilla vel eget mi. Cras efficitur pretium tortor, convallis feugiat lacus efficitur nec. Mauris blandit augue magna, at faucibus mi suscipit a. Interdum et malesuada fames ac ante ipsum primis in faucibus. ",
-		source: Source.all.sample,
-		tags: Tag.all.sample(3),
-		region_id: 1,
-		image: image.sample
-	})
-end
+# (1..20).each do |i|
+# 	NewsItem.create({
+# 		title: "Noticia #{i}",
+# 		url: "http://www.noticia#{1}.cl",
+# 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat vulputate lobortis. Etiam eleifend cursus orci, sed lobortis quam blandit eget. Nulla ac augue risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam tempor lacinia mauris et placerat. Aenean finibus nec magna a pharetra. Morbi at imperdiet risus. Pellentesque tincidunt vestibulum nibh id bibendum. Curabitur nec lobortis nulla. Nullam aliquam vestibulum nisl eu dapibus. Pellentesque quis nulla vitae tortor sollicitudin fringilla vel eget mi. Cras efficitur pretium tortor, convallis feugiat lacus efficitur nec. Mauris blandit augue magna, at faucibus mi suscipit a. Interdum et malesuada fames ac ante ipsum primis in faucibus. ",
+# 		source: Source.all.sample,
+# 		tags: Tag.all.sample(3),
+# 		region_id: 1,
+# 		image: image.sample
+# 	})
+# end
